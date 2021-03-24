ba3c3d3aa4d63ddcc26c0d64690d80e569fd2849
jokerohit15 <81302923+jokerohit15@users.noreply.github.com>
2021-03-24 17:01:24 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.deepOrange,
           visualDensity: VisualDensity.adaptivePlatformDensity,
